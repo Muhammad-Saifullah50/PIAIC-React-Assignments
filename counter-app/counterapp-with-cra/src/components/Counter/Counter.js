498e98e6e50b31db790ebb956c0819cf257dbd45
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-02-27 17:06:23 +0500

    linking counter.js remaining

---
+++ counter-app/counterapp-with-cra/src/components/Counter/Counter.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    const Increment = () => {
+        setCount(count + 1)
+    }
+    const Decrement = () => {
+        setCount(count - 1)
+    }
+    const Reset = () => {
+        setCount(0)
+    }
+    return (<>
+        <div className="container">
+
+            <div className="heading">
+                <h1>Counter App</h1>
+            </div>
+
+            <div className="inputfield">
+                <span>{count}</span>
+            </div>
+
+            <div className="buttons">
+                <button id="inc" onClick={Increment}>Increment ++</button>
+                <button id="dec" onClick={Decrement}
+                    disabled={count === 0}>Decrement --</button>
+                <button id="reset" onClick={Reset}
+                    disabled={count === 0}>Reset</button>
+            </div>
+
+        </div>
+    </>)
+}
