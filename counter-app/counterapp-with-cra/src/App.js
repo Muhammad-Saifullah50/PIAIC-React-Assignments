30ccb8f5dae9c9886f692c0a8e2e698e84e3c73d
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-02-27 19:09:12 +0500

    completed app with cra

--- counter-app/counterapp-with-cra/src/App.js
+++ counter-app/counterapp-with-cra/src/App.js
@@ -1,10 +1,10 @@
 // main logic of application is in this file. we will link external components to this.
-
+import Counter from "./components/Counter/Counter";
 function App() {
 
   return (<>
     <div className="App">
-
+      <Counter />
     </div>
   </>)
 };
--- counter-app/counterapp-with-cra/src/components/Counter/Counter.js
+++ counter-app/counterapp-with-cra/src/components/Counter/Counter.js
@@ -34,3 +34,5 @@ const Counter = () => {
         </div>
     </>)
 }
+
+export default Counter
