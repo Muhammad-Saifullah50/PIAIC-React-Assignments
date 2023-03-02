1fa642e2933e35cf2d5cc944dc913c147542afe4
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-03-02 19:53:33 +0500

    cleaned up mess

--- todo-app/todoapp-with-webpack/src/App.js
+++ todo-app/todoapp-with-webpack/src/App.js
@@ -1,27 +1,11 @@
 import React, { useState } from "react";
+
 const App = () => {
-
-    const [newItem, setNewItem] = useState("");
+    return(
+    <>
     
-    const addItem = () => {
-        console.log(newItem)
-    }
+    </>
+    )
+}
 
-    return (<>
-        <div className="header">
-            <h1>Todo List</h1>
-        </div>
-        <div>
-        <input
-            type="text"
-            placeholder="Enter a task"
-            value={newItem}
-            onChange={e => setNewItem(e.target.value)}
-        />
-        <button onClick={() => addItem} >Add Task</button>
-        </div>
-
-    </>)
-
-    };
 export default App;
