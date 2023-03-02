e4d6522c2b78b0a613e938ed254f374b4776de07
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-03-02 11:58:37 +0500

    followed video till 5:30

--- todo-app/todoapp-with-webpack/src/App.js
+++ todo-app/todoapp-with-webpack/src/App.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 const App = () => {
-return <h1>Hello React</h1>;
-};
+
+    const [newItem, setNewItem] = useState("");
+    
+    const addItem = () => {
+        console.log(newItem)
+    }
+
+    return (<>
+        <div className="header">
+            <h1>Todo List</h1>
+        </div>
+        <div>
+        <input
+            type="text"
+            placeholder="Enter a task"
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+        />
+        <button onClick={() => addItem} >Add Task</button>
+        </div>
+
+    </>)
+
+    };
 export default App;
--- todo-app/todoapp-with-webpack/src/index.js
+++ todo-app/todoapp-with-webpack/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
+import "./App.css";
 const el = document.getElementById("root"); ReactDOM.render(<App />, el);
