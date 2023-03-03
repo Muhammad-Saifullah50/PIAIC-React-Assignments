1edf0aa365e9f613fa8c7daeb373308fb48bd701
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-03-03 22:22:01 +0500

    completed cdn app

---
+++ todo-app/todoapp-with-cdn/index.js
@@ -0,0 +1,65 @@
+let root = document.getElementById("root")
+const { useState } = React
+// import React, { useState } from "react";
+// import { FaTrash } from "react-icons/fa";
+
+
+
+const Todo = () => {
+    const [InputData, setInputData] = useState("");
+
+    const [Items, setItems] = useState([]);
+
+    const addItem = () => {
+        if (!InputData) {
+            alert("Enter a task")
+        }
+        else {
+            setItems([...Items, InputData]);
+            setInputData('');
+        }
+    }
+
+    const deleteitem = (id) => {
+        console.log(id)
+        const updateditems = Items.filter((elem, ind) => {
+            return ind !== id;
+
+        })
+        setItems(updateditems);
+    }
+    return (
+        <>
+            <div className="main">
+                <div className="header">
+                    <h1>Todo List</h1>
+                </div>
+                <div className="form">
+                    <input
+                        type="text"
+                        placeholder="Enter a task..."
+                        value={InputData}
+                        onChange={(e) => setInputData(e.target.value)}
+                    />
+                    <button onClick={addItem} >Add Task</button>
+                </div>
+
+                {
+                    Items.map((elem, ind) => {
+                        return (<>
+                            <div className="showItems" key={ind} >
+                                <div className="list">{elem}</div>
+                                <div className="trashicon" onClick={() => deleteitem(ind)}>
+                                    <img src="icons8-trash-can-64.png" alt="delete" />
+                                </div>
+                            </div>
+                        </>)
+                    })
+
+                }
+
+            </div>
+        </>
+    )
+}
+ReactDOM.render(<Todo />, root)
